8b9347400b2270073cc876087de4d23a207f01ff
Renzo Castro <renzo.vista@gmail.com>
2018-12-15 12:17:03 -0500

    clase 5: add and remove circles 2

--- clase-5/circles-add-remove/app.js
+++ clase-5/circles-add-remove/app.js
@@ -1,27 +1,52 @@
 let container;
 let templateCirculo;
 let btnAgregar;
+let btnEliminar;
 
 const onClickBtnAgregar = event => {
+  event.preventDefault();
+
   const circleContainer = document.createElement('div');
   circleContainer.classList.add('circle-container');
+
   circleContainer.innerHTML = templateCirculo.innerHTML;
+  
   container.appendChild(circleContainer);
 }
+
+const onClickBtnEliminar = event => {
+  event.preventDefault();
+
+  // OPCIÓN 1
+  // const circleContainer = container.querySelector('.circle-container:last-child');
+
+  // OPCIÓN 2
+  const circleContainers = [...container.querySelectorAll('.circle-container')];
+  const circleContainer = circleContainers[circleContainers.length - 1];
+
+  container.removeChild(circleContainer);
+};
 
 const cache = () => {
   container = document.querySelector('#container');
   templateCirculo = document.querySelector('#tpl-circle');
   btnAgregar = document.querySelector('#btn-agregar');
+  btnEliminar = document.querySelector('#btn-eliminar');
 };
 
 const listeners = () => {
   btnAgregar.addEventListener('click', onClickBtnAgregar);
+  btnEliminar.addEventListener('click', onClickBtnEliminar);
+};
+
+const setup = () => {
+  
 };
 
 const init = () => {
   cache();
   listeners();
+  setup();
 };
 
 document.addEventListener('DOMContentLoaded', init);
