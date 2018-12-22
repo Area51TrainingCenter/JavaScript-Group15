771bd36ed62ccbd9ed9fb0868194121c040dd54f
Renzo Castro <renzo.vista@gmail.com>
2018-12-22 10:41:46 -0500

    clase 6: webpack parte 2

--- clase-6/webpack--import-export/src/index.js
+++ clase-6/webpack--import-export/src/index.js
@@ -1,2 +1,16 @@
+import { sumar, multiplicar } from './libs/utils';
+
 const saludo = 'Hola mundo!';
 console.log(`saludo: ${saludo}`);
+
+
+let resultado
+
+resultado = sumar(3, 5);
+console.log(resultado);
+
+resultado = multiplicar(3, 5);
+console.log(resultado);
+
+
+
