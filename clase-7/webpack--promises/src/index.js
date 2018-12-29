c6a2dc42a4b59f9f249b480876d6bc970498fa50
Renzo Castro <renzo.vista@gmail.com>
2018-12-29 11:29:32 -0500

    clase 7: promise THEN CATCH ALL

--- clase-7/webpack--promises/src/index.js
+++ clase-7/webpack--promises/src/index.js
@@ -39,7 +39,6 @@ setTimeout(() => {
 
 // .then
 // .catch
-// .when
 
 
 const sumar = (a, b) => {
@@ -62,3 +61,84 @@ promesaSumar(3, 5)
   .then((resultado) => {
     console.log('SUMA', resultado);
   });
+
+// diaSemana(0)   // Domingo
+// diaSemana(1)   // Lunes
+// diaSemana(2)   // Martes
+
+
+
+const diaSemana = indice => {
+  // const dias = ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'];
+  const dias = 'Domingo,Lunes,Martes,Miércoles,Jueves,Viernes,Sábado'.split(',');
+
+  return dias[indice];
+};
+
+const diaSemanaPromesa = indice => {
+  return new Promise((resolve, reject) => {
+
+    setTimeout(() => {
+      const dias = 'Domingo,Lunes,Martes,Miércoles,Jueves,Viernes,Sábado'.split(',');
+
+      resolve(dias[indice]);
+    }, 3000);
+
+  });
+};
+
+console.log(diaSemana(3));
+diaSemanaPromesa(3).then(dia => console.log(dia));
+
+
+
+const sistemaIntermitente = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const tieneFalla = ((new Date()).getSeconds() % 2 == 0);
+
+      if (tieneFalla) {
+        reject(0);
+      } else {
+        resolve(1);
+      }
+    }, 3000)
+  });
+}
+// DEMO 1
+sistemaIntermitente()
+  .then(() => {
+    console.log('SIN ERRORES');
+  })
+  .catch(() => {
+    console.log('CON ERRORES');
+  });
+
+
+// DEMO 2
+sistemaIntermitente()
+  .then((response) => {
+    console.log('SIN ERRORES', response);
+
+    return 5; // Pasamos este dato al siguiente THEN en su parametro "response"
+    // Esto mismo usabamos en la primeras líneas con FETCH
+  })
+  .then((response) => {
+    console.log('SIN ERRORES', response);
+  })
+  .catch((error) => {
+    console.log('CON ERRORES', error);
+  });
+
+
+// DEMO 3
+Promise.all([
+  sistemaIntermitente(),
+  sistemaIntermitente(),
+  sistemaIntermitente()
+])
+  // .then(function () {
+  //   console.log('arguments', arguments)
+  // })
+  .then(response => console.log('OK arguments', response[0], response[1], response[2]))
+  .catch(err => console.log('ERROR arguments', err));
