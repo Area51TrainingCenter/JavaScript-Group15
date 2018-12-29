65a2a8b59c98ed6c7d5ef62301056f09cd9db688
Renzo Castro <renzo.vista@gmail.com>
2018-12-29 12:19:39 -0500

    clase 7: async await EJERCICIO

--- clase-7/webpack--async-await/src/index.js
+++ clase-7/webpack--async-await/src/index.js
@@ -41,6 +41,38 @@ init();
 */
 
 
-// Promise
-// - Paralelo
-// - Secuencial
+// Promise, tipos de tareas
+// - Paralela
+// - Secuencial o Serial
+
+
+
+
+const intermitente = () => ((new Date()).getSeconds() % 2 === 0);
+
+const validarSesion = () => new Promise((resolve, reject) => {
+  if (intermitente()) {
+    resolve();
+  } else {
+    reject();
+  }
+});
+
+const login = (user, password) => new Promise((resolve, reject) => {
+  if (user === 'alumno' && password === '123456') {
+    resolve();
+  } else {
+    reject();
+  }
+});
+
+const mostrarDatos = () => new Promise((resolve, reject) => {
+  resolve();
+});
+
+
+// 1. validarSesion
+// 2. login
+// 3. mostrarDatos
+
+
